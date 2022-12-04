f8e7228616111e69517e7adc0603703b3aed3b15
Karlo Palisoc <karlobpalisoc@gmail.com>
2022-12-04 11:34:00 +0800

    first commit

---
+++ src/routes/(main)/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Nav from '../../components/Nav.svelte';
+	import '../../app.css';
+</script>
+
+<div>
+	<div>
+		<Nav />
+	</div>
+
+	<main class="m-auto h-full w-[952px] ">
+		<slot />
+	</main>
+</div>
---
+++ src/routes/(post)/sample-page/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Nav from '../../../components/Nav.svelte';
+	import '../../../app.css';
+</script>
+
+<div>
+	<div>
+		<Nav />
+	</div>
+	<slot />
+</div>
